4f6c96a8da07c17f7d8a9ff47ada8180d0fccfc0
hibaMr <156079182+hibaMr@users.noreply.github.com>
2024-01-30 11:06:35 +0100

    first test

---
+++ seance-1/index.js
@@ -0,0 +1,38 @@
+console.log("hellow javascript")            // hadi hiya print fi js
+
+
+// prosedure
+
+// function greet(nom){
+//     console.log("hellew" + nom)
+// }
+// greet(" laiela")
+
+
+// fonction
+
+
+// function somme(num1,num2){
+//     return num1 + num2
+// }
+// console.log(somme(13,80))
+
+
+
+
+const PI = 3.14                 // consetante
+function inscrire(){
+    var nom;                        //variable  ==> imkene lina ndiro var fi blasete let mais likhetilafe li binathome howa let kate3erafe 4ire fi lbloc dyalaha ila ketebna fi console.log chi haja khareja fi bloc akhere makat3erafehache 3akesse var fine ma kanete kate3arefe 3eliha   !"hadike let katkhedame "
+    var prenom;
+    var age;
+    nom = "mribeh"
+    prenom = "hiba"
+    age = 21
+    if(age < 22){                         //condition
+        var statut;
+        statut = "stagiaire"
+    }
+    console.log(nom)
+    console.log(statut)
+}
+inscrire()
---
+++ seance-2/main.js
@@ -0,0 +1,42 @@
+// comparé
+let a = 5;
+let b = "5";
+ if(a === b){                    //ila derena (==) katbane lina 55 hite hade (==) katcompari lina 2 dyale les valeur blama tcompari lina type dyalhome mais (===)ila derenaha katcompari lina les valeur o type dyalhome hite hena 3adena 5 number o '5' caractaire fi hade lhala ma4adi ibane lina walo
+    console.log("les deux valuers sont comparé")
+}else console.log("les deux valuers sont diffirence");
+
+
+
+
+// diffirence
+let c = 5;
+let d = "5";
+ if(a !== b){                    // (!==)hadi katcompari lina les deux valeur m3a type dyalhome o (!=)katcompari lina 4ire les valeur bla type
+    console.log("les deux valuers sont diffirence")
+}else{ 
+    console.log("les deux valuers sont comparé")
+}
+
+
+// opération logique
+
+// and ==> &&
+let statut = "etudient" ;
+let age = 20;
+if(age == 20 && statut == "etudient"){
+    console.log("ok")
+}else{ 
+    console.log("non")
+}
+
+
+// or ==> ||
+let numero = 20;
+let etudient = "hiba"
+if(numero == 20 || etudient == "hiba"){
+    console.log("ok")
+}else{
+    console.log("non")
+}
+
+

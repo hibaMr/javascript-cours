56e43b64776e451b37c1a3979348a9713fe6c661
hibaMr <156079182+hibaMr@users.noreply.github.com>
2024-05-01 20:25:07 +0200

    projet-les-photo-JS

---
+++ projet-1/main.js
@@ -0,0 +1,373 @@
+let container = document.createElement('div')
+
+container.style.cssText = `
+width: 100%;
+display: flex;
+flex-wrap:wrap;
+justify-content:center;
+`
+
+
+let box1 = document.createElement('div')
+let box2 = document.createElement('div')
+let box3 = document.createElement('div')
+let box4 = document.createElement('div')
+let box5 = document.createElement('div')
+let box6 = document.createElement('div')
+let box7 = document.createElement('div')
+let box8 = document.createElement('div')
+let box9 = document.createElement('div')
+let box10 = document.createElement('div')
+
+
+box1.style.cssText = `
+width : 200px;
+background-color: black;
+margin:5px;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+box2.style.cssText = `
+width : 200px;
+background-color: black;
+margin:5px;
+display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+box3.style.cssText = `
+width : 200px;
+background-color: black;
+margin:5px;
+display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+box4.style.cssText = `
+width : 200px;
+background-color: black;
+margin:5px;
+display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+box5.style.cssText = `
+width : 200px;
+background-color: black;
+margin:5px;
+display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+box6.style.cssText = `
+width : 200px;
+background-color: black;
+margin:5px;
+display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+box7.style.cssText = `
+width : 200px;
+background-color: black;
+margin:5px;
+display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+box8.style.cssText = `
+width : 200px;
+background-color: black;
+margin:5px;
+display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+box9.style.cssText = `
+width : 200px;
+background-color: black;
+margin:5px;
+display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+box10.style.cssText = `
+width : 200px;
+background-color: black;
+margin:5px;
+display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+box1.style.background = "black"
+
+let title1 = document.createElement('h1')
+let title2 = document.createElement('h1')
+let title3 = document.createElement('h1')
+let title4 = document.createElement('h1')
+let title5 = document.createElement('h1')
+let title6 = document.createElement('h1')
+let title7 = document.createElement('h1')
+let title8 = document.createElement('h1')
+let title9 = document.createElement('h1')
+let title10 = document.createElement('h1')
+
+let para1 = document.createElement('p')
+let para2 = document.createElement('p')
+let para3 = document.createElement('p')
+let para4 = document.createElement('p')
+let para5 = document.createElement('p')
+let para6 = document.createElement('p')
+let para7 = document.createElement('p')
+let para8 = document.createElement('p')
+let para9 = document.createElement('p')
+let para10 = document.createElement('p')
+
+let img1 = document.createElement('img')
+let img2 = document.createElement('img')
+let img3 = document.createElement('img')
+let img4 = document.createElement('img')
+let img5 = document.createElement('img')
+let img6 = document.createElement('img')
+let img7 = document.createElement('img')
+let img8 = document.createElement('img')
+let img9 = document.createElement('img')
+let img10 = document.createElement('img')
+
+img1.style.cssText = `
+width:170px;
+taxt-aling:center;
+margin-bottom:10px;
+`
+img2.style.cssText = `
+width:170px;
+taxt-aling:center;
+margin-bottom:10px;
+`
+img3.style.cssText = `
+width:170px;
+taxt-aling:center;
+margin-bottom:10px;
+`
+img4.style.cssText = `
+width:170px;
+taxt-aling:center;
+margin-bottom:10px;
+`
+img5.style.cssText = `
+width:170px;
+taxt-aling:center;
+margin-bottom:10px;
+`
+img6.style.cssText = `
+width:170px;
+taxt-aling:center;
+margin-bottom:10px;
+`
+img7.style.cssText = `
+width:170px;
+taxt-aling:center;
+margin-bottom:10px;
+`
+img8.style.cssText = `
+width:170px;
+taxt-aling:center;
+margin-bottom:10px;
+`
+img9.style.cssText = `
+width:170px;
+taxt-aling:center;
+margin-bottom:10px;
+`
+img10.style.cssText = `
+width:170px;
+taxt-aling:center;
+margin-bottom:10px;
+`
+
+img1.src = 'img.jpg'
+img2.src = 'img.jpg'
+img3.src = 'img.jpg'
+img4.src = 'img.jpg'
+img5.src = 'img.jpg'
+img6.src = 'img.jpg'
+img7.src = 'img.jpg'
+img8.src = 'img.jpg'
+img9.src = 'img.jpg'
+img10.src = 'img.jpg'
+
+let textTitle1 = document.createTextNode('ali')
+let textTitle2 = document.createTextNode('ahmed')
+let textTitle3 = document.createTextNode('abdelrahman')
+let textTitle4 = document.createTextNode('hiba')
+let textTitle5 = document.createTextNode('ali')
+let textTitle6 = document.createTextNode('abdelrahman')
+let textTitle7 = document.createTextNode('ahmed')
+let textTitle8 = document.createTextNode('naoual')
+let textTitle9 = document.createTextNode('hiba')
+let textTitle10 = document.createTextNode('naoual')
+
+title1.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+title2.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+title3.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+title4.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+title5.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+title6.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+title7.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+title8.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+title9.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+title10.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+
+let textPara1 = document.createTextNode('20 years old')
+let textPara2 = document.createTextNode('19 years old')
+let textPara3 = document.createTextNode('20 years old')
+let textPara4 = document.createTextNode('18 years old')
+let textPara5 = document.createTextNode('30 years old')
+let textPara6 = document.createTextNode('20 years old')
+let textPara7 = document.createTextNode('12 years old')
+let textPara8 = document.createTextNode('28 years old')
+let textPara9 = document.createTextNode('40 years old')
+let textPara10 = document.createTextNode('21 years old')
+
+
+para1.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+para2.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+para3.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+para4.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+para5.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+para6.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+para7.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+para8.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+para9.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+para10.style.cssText = `
+color:white;
+taxt-aling:center;
+`
+
+title1.appendChild(textTitle1);
+title2.appendChild(textTitle2);
+title3.appendChild(textTitle3);
+title4.appendChild(textTitle4);
+title5.appendChild(textTitle5);
+title6.appendChild(textTitle6);
+title7.appendChild(textTitle7);
+title8.appendChild(textTitle8);
+title9.appendChild(textTitle9);
+title10.appendChild(textTitle10);
+
+para1.appendChild(textPara1);
+para2.appendChild(textPara2);
+para3.appendChild(textPara3);
+para4.appendChild(textPara4);
+para5.appendChild(textPara5);
+para6.appendChild(textPara6);
+para7.appendChild(textPara7);
+para8.appendChild(textPara8);
+para9.appendChild(textPara9);
+para10.appendChild(textPara10);
+
+
+box1.appendChild(para1);
+box2.appendChild(para2);
+box3.appendChild(para3);
+box4.appendChild(para4);
+box5.appendChild(para5);
+box6.appendChild(para6);
+box7.appendChild(para7);
+box8.appendChild(para8);
+box9.appendChild(para9);
+box10.appendChild(para10);
+
+box1.appendChild(title1);
+box2.appendChild(title2);
+box3.appendChild(title3);
+box4.appendChild(title4);
+box5.appendChild(title5);
+box6.appendChild(title6);
+box7.appendChild(title7);
+box8.appendChild(title8);
+box9.appendChild(title9);
+box10.appendChild(title10);
+
+box1.appendChild(img1);
+box2.appendChild(img2);
+box3.appendChild(img3);
+box4.appendChild(img4);
+box5.appendChild(img5);
+box6.appendChild(img6);
+box7.appendChild(img7);
+box8.appendChild(img8);
+box9.appendChild(img9);
+box10.appendChild(img10);
+
+container.appendChild(box1);
+container.appendChild(box2);
+container.appendChild(box3);
+container.appendChild(box4);
+container.appendChild(box5);
+container.appendChild(box6);
+container.appendChild(box7);
+container.appendChild(box8);
+container.appendChild(box9);
+container.appendChild(box10);
+
+document.body.appendChild(container)

4d377bb1d766315a2aebba89ba3fc326f2b60513
hibaMr <156079182+hibaMr@users.noreply.github.com>
2024-05-01 20:29:48 +0200

    projet-form

---
+++ projet-form/script.js
@@ -0,0 +1,10 @@
+let box_1 = document.querySelector(".box")
+
+let box2 = document.querySelector(".none")
+
+function login(){
+    box2.classList.remove("none")
+
+    box_1.classList.add("none")
+    console.log("hello")
+}
